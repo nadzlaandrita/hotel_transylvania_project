2991df144fa2b775574616a85479112f036b30c3
Vincent <115674966+Lukito2002@users.noreply.github.com>
2023-01-23 16:57:07 +0700

    halaman home member kelarrr
    tinggal extend navbar aja

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home-member', function () {
+    return view('.member/home-member');
+});
+
