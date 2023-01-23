c6b6788bdebebf13ffa69dd13b662ae2967176b4
nadzlaandrita <nadzlaandrita18@gmail.com>
2023-01-23 19:33:23 +0700

    cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Models\Room;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-    //
+    public function addCart(Request $request, $room_id){
+        Room::find($request->room_id);
+
+        $request->validate([
+            'checkin_date' => 'required|date|date_format:Y-m-d H:i:s',
+            'checkout_date' => 'required|date|date_format:Y-m-d H:i:s|after:checkin_date'
+        ]);
+
+        $cart_data = Cart::all()->where('user_id', '=', Auth::user()->id)->where('room_id', '=', $room_id)->first();
+
+        if($cart_data == null){
+            Cart::insert([
+                'user_id' => Auth::user()->id,
+                'room_id' => $room_id,
+                'booked_from' => $request->checkin_date,
+                'booked_until' => $request->checkout_date
+            ]);
+        }
+
+        return redirect('/');
+
+    }
+
+    public function removeCart($room_id){
+        $cart_data = Cart::where('room_id', '=', $room_id);
+        $cart_data->delete();
+        return redirect ('/');
+    }
+
+    public function editCart(Request $request, $room_id){
+        Room::find($request->room_id);
+
+        $request->validate([
+            'checkin_date' => 'required|date|date_format:Y-m-d H:i:s',
+            'checkout_date' => 'required|date|date_format:Y-m-d H:i:s|after:checkin_date'
+        ]);
+
+        $cart_data = Cart::all()->where('user_id', '=', Auth::user()->id)->where('room_id', '=', $room_id)->first();
+
+        $cart_data->update([
+            'booked_from' => $request->checkin_date,
+            'booked_until' => $request->checkout_date
+        ]);
+
+        return redirect('/');
+    }
+
+    
 }
